d374223ac085d10503c28a205e2780732c87a049
Andrew Newdigate <andrew@troupe.co>
2015-02-22 17:42:00 +0000

    Support for pull-requests

---
+++ test/resources/pullRequest.js
@@ -0,0 +1,76 @@
+var GHClient = require('../..');
+var assert = require('assert');
+
+describe('pullRequest', function() {
+  var ghClient;
+
+  before(function() {
+    ghClient = new GHClient({
+      accessToken: process.env.GITHUB_ACCESS_TOKEN
+    });
+  });
+
+  it('listForRepo', function(done) {
+    ghClient.pullRequest.listForRepo('gitterHQ/services', { query: { state: 'all' } })
+      .spread(function(pulls) {
+        assert(Array.isArray(pulls));
+        assert(pulls.length > 0);
+      })
+      .nodeify(done);
+  });
+
+  it('listForRepo closed', function(done) {
+    ghClient.pullRequest.listForRepo('gitterHQ/services', { query: { state: 'open' } })
+      .spread(function(pulls) {
+        assert(Array.isArray(pulls));
+        pulls.forEach(function(pull) {
+          assert.strictEqual(pull.state, 'open');
+        });
+      })
+      .nodeify(done);
+  });
+
+  it('get', function(done) {
+    ghClient.pullRequest.get('gitterHQ/services', 3)
+      .spread(function(pull) {
+        assert(pull);
+      })
+      .nodeify(done);
+  });
+
+  it('get non-existant', function(done) {
+    ghClient.pullRequest.get('gitterHQ/services231', 3)
+      .spread(function(pull) {
+        assert(!pull);
+      })
+      .nodeify(done);
+  });
+
+  it('listCommits', function(done) {
+    ghClient.pullRequest.listCommits('gitterHQ/services', 3)
+      .spread(function(commits) {
+        assert(Array.isArray(commits));
+        assert(commits.length > 0);
+      })
+      .nodeify(done);
+  });
+
+  it('listFiles', function(done) {
+    ghClient.pullRequest.listFiles('gitterHQ/services', 3)
+      .spread(function(files) {
+        assert(Array.isArray(files));
+        assert(files.length > 0);
+      })
+      .nodeify(done);
+  });
+
+  it('checkMerged', function(done) {
+    ghClient.pullRequest.checkMerged('gitterHQ/services', 3)
+      .spread(function(isMerged) {
+        assert(isMerged);
+      })
+      .nodeify(done);
+  });
+
+
+});
