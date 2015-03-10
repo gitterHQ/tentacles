3ec078d1c3d1baecbea0e6ec9586512ddf377ae2
Andrew Newdigate <andrew@troupe.co>
2015-03-10 17:12:51 +0200

    Support for request extensions

---
+++ test/extensions.js
@@ -0,0 +1,21 @@
+var GHClient = require('..');
+var assert = require('assert');
+
+describe('extensions', function() {
+
+  it('should allow extensions to be specified', function(done) {
+    var ghClient = new GHClient({
+      extensions: [function(options, callback/*, next */) {
+        /* Shortcut extension */
+        callback(null, { statusCode: 200 }, { fakeUser: true });
+      }]
+    });
+
+    ghClient.user.getAuthUser()
+      .then(function(body) {
+        assert.deepEqual(body, { fakeUser: true });
+      })
+      .nodeify(done);
+  });
+
+});
